7dfb910ec0a828545cef60a3c403af397d20b78d
Vitalii Zaidenshnir <sapsan281993@gmail.com>
2025-01-29 11:03:09 +0100

    finish

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import iziToast from 'izitoast/dist/js/iziToast.min.js';
+import 'izitoast/dist/css/iziToast.min.css';
+import { fetchImages } from './js/pixabay-api.js';
+
+export const refs = {
+  form: document.querySelector('.form'),
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
+
+refs.form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const inputValue = form.elements.state.value.trim();
+
+  refs.gallery.innerHTML = '';
+
+  if (!inputValue) {
+    iziToast.error({
+      message: 'Please enter your request',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  refs.loader.classList.remove('is-hidden');
+
+  fetchImages(inputValue)
+    .catch(error => {
+      iziToast.error({
+        message: 'Error fetching images. Please try again later.',
+        position: 'topRight',
+      });
+      console.error(error);
+    })
+    .finally(() => {
+      refs.loader.classList.add('is-hidden');
+    });
+
+  refs.form.reset();
+}
